48b96ee2154a1c2e5d1ad6bb3803a86172a4daac
Manuel Spartan <spartantri@gmail.com>
2020-08-24 02:02:20 -0400

    Added conditional SIFT installation

--- EC2 Auto Clean Room Forensics/Lambda-Functions/runForensicAnalysis.py
+++ EC2 Auto Clean Room Forensics/Lambda-Functions/runForensicAnalysis.py
@@ -25,16 +25,16 @@ def lambda_handler(event, context):
     instanceID = event['instanceID']
     S3BucketName = os.environ['OUTPUT_S3_BUCKETNAME']
     S3BucketRegion = os.environ['OUTPUT_S3_BUCKETREGION']
-    commands = ['#!/bin/bash','date -u +"%Y-%m-%dT%H:%M:%SZ"',
+    InitialSetup = ['#!/bin/bash','date -u +"%Y-%m-%dT%H:%M:%SZ"',
                 'sudo mkfs /dev/xvdg','sudo mkdir /forensics','sudo mount /dev/xvdg /forensics',
-                'sudo apt install sleuthkit -y',
-                'sudo curl -Lo /usr/local/bin/sift https://github.com/sans-dfir/sift-cli/releases/download/v1.8.5/sift-cli-linux',
-                'sudo chmod +x /usr/local/bin/sift', 'sudo sift install --mode=server', 'sudo sift update',
-                'sudo dd if=/dev/xvdf1 of=/forensics/' + instanceID + '.dd',
+                'sudo apt install sleuthkit -y']
+    SIFTinstall = ['sudo curl -Lo /usr/local/bin/sift https://github.com/sans-dfir/sift-cli/releases/download/v1.8.5/sift-cli-linux',
+                'sudo chmod +x /usr/local/bin/sift', 'sudo sift install --mode=server', 'sudo sift update']
+    Forensicate = ['sudo dd if=/dev/xvdf1 of=/forensics/' + instanceID + '.dd',
                 'sudo fls -r -m -i /forensics/' + instanceID + '.dd >~/file-full-' + instanceID + '.txt',
                 'sudo mactime -b ~/file-full-' + instanceID + '.txt $date >~/file-mac-' + instanceID + '.txt',
-                'sudo fls -rd /forensics/' + instanceID + '.dd >~/file-deleted-' + instanceID + '.txt',
-                'sudo apt-get install cloud-utils -y',
+                'sudo fls -rd /forensics/' + instanceID + '.dd >~/file-deleted-' + instanceID + '.txt']
+    Reporting = ['sudo apt-get install cloud-utils -y',
                 'EC2_INSTANCE_ID=$(ec2metadata --instance-id)',
                 'cp ~/file-deleted-' + instanceID + '.txt ~/file-deleted-$EC2_INSTANCE_ID-' + instanceID+ '.txt',
                 'cp ~/file-mac-' + instanceID + '.txt ~/$EC2_INSTANCE_ID.txt',
@@ -42,6 +42,10 @@ def lambda_handler(event, context):
                 'aws s3 cp ~/file-full-' + instanceID + '.txt s3://' + S3BucketName + '/incident-response/' + instanceID + '/',
                 'aws s3 cp ~/file-deleted-' + instanceID + '-' + instanceID + '.txt s3://' + S3BucketName + '/incident-response/' + instanceID+ '/',
                 'aws s3 cp ~/file-mac-' + instanceID + '.txt s3://' + S3BucketName +'/incident-response/' + instanceID + '/']
+    commands = InitialSetup + Forensicate + Reporting
+    if 'InstallSIFT' in os.environ:
+        if os.environ['InstallSIFT'].lower() == "yes":
+            commands = InitialSetup + SIFTinstall + Forensicate + Reporting
     
     
     response = ssmclient.send_command(
